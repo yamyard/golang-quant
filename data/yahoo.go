19a4be36dc70bd72b4f16e248fe71847a1395fdc
yamyard <yamyard@hotmail.com>
2025-06-26 15:49:23 +0800

    Update yahoo.go

--- data/yahoo.go
+++ data/yahoo.go
@@ -1,5 +1,6 @@
 package data
 
 func FetchYahooData(symbol string) ([]float64, error) {
-    return []float64{100, 101, 102, 103, 104}, nil     // 目前为演示用途，直接返回固定的价格序列
+    // 目前为演示用途，直接返回固定的价格序列
+    return []float64{100, 101, 102, 103, 104}, nil
 }
