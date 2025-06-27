cb0d31d35d2745b1ae89461acee8707c7d2a53fb
yamyard <yamyard@hotmail.com>
2025-06-27 12:24:15 +0800

    Update engine.go

--- backtest/engine.go
+++ backtest/engine.go
@@ -11,10 +11,10 @@ func RunBacktest(prices []float64, signals []string) float64 {
     // 遍历价格和信号序列
     for i := range prices {
         if signals[i] == "BUY" {
-            // 收到买入信号，按当前价格买入 1 单位
+            // 收到买入信号，按当前价格买入 1 单位/股
             b.Execute(broker.Order{Type: "BUY", Price: prices[i], Amount: 1})
         } else if signals[i] == "SELL" {
-            // 收到卖出信号，按当前价格卖出 1 单位
+            // 收到卖出信号，按当前价格卖出 1 单位/股
             b.Execute(broker.Order{Type: "SELL", Price: prices[i], Amount: 1})
         }
         // 其他信号不做操作
